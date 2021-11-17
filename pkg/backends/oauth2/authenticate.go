28ad5dd9443e8797364c1151573617ac20228baa
Joao Silva <joao2@scottytechnologies.com>
2021-11-17 12:32:23 +0000

    Fix scopes

--- pkg/backends/oauth2/authenticate.go
+++ pkg/backends/oauth2/authenticate.go
@@ -129,8 +129,7 @@ func (b *Backend) Authenticate(r *requests.Request) error {
 			switch b.Config.Provider {
 			case "google":
 				if b.Config.ScopeExists(
-					"https://www.googleapis.com/auth/cloud-identity.groups.readonly",
-					"cloud-identity.groups.readonly",
+					"https://www.googleapis.com/auth/groups",
 				) {
 					if err := b.fetchUserGroups(accessToken, m); err != nil {
 						return errors.ErrBackendOauthFetchUserGroupsFailed.WithArgs(err)
